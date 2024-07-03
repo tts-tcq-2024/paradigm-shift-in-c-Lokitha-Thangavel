8b9a6fea8820e6c60d80a98e69e5fcbb65fcda7d
Lokitha-Thangavel <69999554+Lokitha-Thangavel@users.noreply.github.com>
2024-07-03 09:48:18 +0530

    Added test cases

--- .github/workflows/checker.c
+++ .github/workflows/checker.c
@@ -15,4 +15,10 @@ int main()
 {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
+  assert(batteryIsOk(0, 20, 0.7));
+  assert(!batteryIsOk(45, 80, 0));
+  assert(batteryIsOk(25, 20, 0.8));
+  assert(!batteryIsOk(50, 80, 0));
+  assert(batteryIsOk(25, 80, 0.7));
+  assert(!batteryIsOk(50, 75, 0.9));
 }
