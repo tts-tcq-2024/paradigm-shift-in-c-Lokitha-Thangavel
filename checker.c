080f888082dc47374a100cb607f0e1e9ce1a35b8
Lokitha-Thangavel <69999554+Lokitha-Thangavel@users.noreply.github.com>
2024-07-23 09:19:15 +0530

    Update checker for warning tolerance

--- checker.c
+++ checker.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
 #include <assert.h>
 
-
-int isInRange(float value, float min, float max) {
-    return (value >= min && value <= max);
+void printMessage(const char * batteryParameter, const char * message)
+{
+    printf("\n%s : %s", batteryParameter, message);
 }
 
-int batteryIsOk(float temperature, float soc, float chargeRate) {
-    return isInRange(temperature, 0, 45) && isInRange(soc, 20, 80) && (chargeRate <= 0.8);
+void checkTolerance(float value, float min, float max, const char * batteryParameter)
+{
+    float warningTolerance = (max * 5) / 100;
+    if((value <= (min + warningTolerance)) || (value >= (max - warningTolerance)))
+    {
+        printMessage(batteryParameter, "Warning Tolerance limit reached");
+    }
 }
 
+int isInRange(float value, float min, float max, const char * batteryParameter) 
+{
+    int calculatedValue = (value >= min && value <= max);
+    if(!calculatedValue)
+    {
+        printMessage(batteryParameter, "Out of Range");
+    }
+    else
+    {
+        checkTolerance(value, min, max, batteryParameter);
+    }
+    return calculatedValue;
+}
+
+int batteryIsOk(float temperature, float soc, float chargeRate) 
+{
+    return isInRange(temperature, 0, 45, "Temperature") && isInRange(soc, 20, 80, "SoC") && isInRange(chargeRate, 0, 0.8, "ChargeRate");
+}
 
 int main() 
 {
